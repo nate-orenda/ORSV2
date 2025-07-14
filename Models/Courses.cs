3c40f841136a207162ccae5c148e84436db115ac
nate-orenda <nate@orendaed.org>
2025-07-14 15:28:07 -0700

    Fixed models courses and gaagprogress

--- Models/Courses.cs
+++ Models/Courses.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace ORSV2.Models
 {
     public class Courses
@@ -7,16 +9,40 @@ namespace ORSV2.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)] // Aeries/Python sets this
         public int Id { get; set; }
+        
         public int DistrictId { get; set; }
-        public string CourseNumber { get; set; }
-        public string Title { get; set; }
-        public string DepartmentCode { get; set; }
-        public string CSU_Rule_ValidationLevelCode { get; set; }
-        public string CSU_SubjectAreaCode { get; set; }
-        public string UC_SubjectAreaCode { get; set; }
-        public string UC_Rule_CanBeAnElective { get; set; }
+        
+        // Required fields - these should always have values
+        [Required]
+        [MaxLength(50)]
+        public required string CourseNumber { get; set; }
+        
+        [Required]
+        [MaxLength(200)]
+        public required string Title { get; set; }
+        
+        // Optional fields - these can be null in the database
+        [MaxLength(50)]
+        public string? DepartmentCode { get; set; }
+        
+        [MaxLength(10)]
+        public string? CSU_Rule_ValidationLevelCode { get; set; }
+        
+        [MaxLength(10)]
+        public string? CSU_SubjectAreaCode { get; set; }
+        
+        [MaxLength(10)]
+        public string? UC_SubjectAreaCode { get; set; }
+        
+        [MaxLength(10)]
+        public string? UC_Rule_CanBeAnElective { get; set; }
+        
+        [Precision(5, 2)]
         public decimal? CreditDefault { get; set; }
-        public string InactiveStatusCode { get; set; }
+        
+        [MaxLength(10)]
+        public string? InactiveStatusCode { get; set; }
+        
         public DateTime? DateUpdated { get; set; }
     }
 }
--- Models/GAAGProgress.cs
+++ Models/GAAGProgress.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace ORSV2.Models
 {
@@ -14,36 +15,64 @@ namespace ORSV2.Models
         public int CP { get; set; }
         public int SchoolYear { get; set; }
 
+        [Precision(5, 2)]
         public decimal? CreditsEarned_ELA { get; set; }
+        [Precision(5, 2)]
         public decimal? CreditsEarned_MATH { get; set; }
+        [Precision(5, 2)]
         public decimal? CreditsEarned_SCI { get; set; }
+        [Precision(5, 2)]
         public decimal? CreditsEarned_FL { get; set; }
+        [Precision(5, 2)]
         public decimal? CreditsEarned_VA { get; set; }
+        [Precision(5, 2)]
         public decimal? CreditsEarned_HIS { get; set; }
+        [Precision(5, 2)]
         public decimal? CreditsEarned_PREP { get; set; }
 
+        [Precision(5, 2)]
         public decimal? CreditsScheduled_ELA { get; set; }
+        [Precision(5, 2)]
         public decimal? CreditsScheduled_MATH { get; set; }
+        [Precision(5, 2)]
         public decimal? CreditsScheduled_SCI { get; set; }
+        [Precision(5, 2)]
         public decimal? CreditsScheduled_FL { get; set; }
+        [Precision(5, 2)]
         public decimal? CreditsScheduled_VA { get; set; }
+        [Precision(5, 2)]
         public decimal? CreditsScheduled_HIS { get; set; }
+        [Precision(5, 2)]
         public decimal? CreditsScheduled_PREP { get; set; }
 
+        [Precision(5, 2)]
         public decimal? TargetEarned_ELA { get; set; }
+        [Precision(5, 2)]
         public decimal? TargetEarned_MATH { get; set; }
+        [Precision(5, 2)]
         public decimal? TargetEarned_SCI { get; set; }
+        [Precision(5, 2)]
         public decimal? TargetEarned_FL { get; set; }
+        [Precision(5, 2)]
         public decimal? TargetEarned_VA { get; set; }
+        [Precision(5, 2)]
         public decimal? TargetEarned_HIS { get; set; }
+        [Precision(5, 2)]
         public decimal? TargetEarned_PREP { get; set; }
 
+        [Precision(5, 2)]
         public decimal? TargetScheduled_ELA { get; set; }
+        [Precision(5, 2)]
         public decimal? TargetScheduled_MATH { get; set; }
+        [Precision(5, 2)]
         public decimal? TargetScheduled_SCI { get; set; }
+        [Precision(5, 2)]
         public decimal? TargetScheduled_FL { get; set; }
+        [Precision(5, 2)]
         public decimal? TargetScheduled_VA { get; set; }
+        [Precision(5, 2)]
         public decimal? TargetScheduled_HIS { get; set; }
+        [Precision(5, 2)]
         public decimal? TargetScheduled_PREP { get; set; }
 
         public bool? MetAllGradeBenchmarks { get; set; }
